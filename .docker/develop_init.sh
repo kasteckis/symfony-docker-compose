78af425d660868c2af0ae65331974681d4258ce7
Valentinas Kasteckis <41127259+kasteckis@users.noreply.github.com>
2020-09-07 19:11:53 +0300

    Update develop_init.sh

--- .docker/develop_init.sh
+++ .docker/develop_init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sleep 10
 cd /var/www/html
-composer install
+# composer install # Uncomment this if you have composer already setup
 # php bin/console d:s:u --force # Uncomment this if you want to update database scheme on every docker compose up
 # php bin/console doctrine:fixtures:load --append # Uncomment this if you want to add fixtures on every docker compose up
 echo "Project has been started"
@@ -9,4 +9,4 @@ echo "Access project website - http://192.168.2.2/"
 echo "MYSQL Server - 192.168.2.3:3306"
 echo "Access phpmyadmin via - http://192.168.2.4/"
 echo "Access email server via - http://192.168.2.5:8025/"
-echo "Access server SSH by writing - docker exec -it quizzer_web bash"
+echo "Access server SSH by writing - docker exec -it project_web bash"
