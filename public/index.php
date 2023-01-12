91da633b99b551c54ef8b56ddcb97d6418feeca7
kasteckis <valentinas@kasteckis.lt>
2023-01-12 18:48:52 +0200

    Fix xdebug on Windows WSL

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "You should put some breakpoints in your IDE";
+
+$something = 4 + 5;
+
+echo " to check if XDEBUG works." . PHP_EOL;
+
+$somethingAgain = 9 + 5 + $something;
+
+echo "I can assure you it works on my machine (:";
