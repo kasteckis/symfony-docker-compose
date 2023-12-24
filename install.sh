fa571ad6879cc1da682023d5d99c28280b483b1d
valentinas <valentinas.kasteckis@bitcat.dev>
2023-12-24 17:40:24 +0200

    Update install.sh script

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-mv ./symfony-docker-compose/.docker ./
-mv ./symfony-docker-compose/Makefile ./
-mv ./symfony-docker-compose/docker-compose.yml ./
-mv ./symfony-docker-compose/docker-compose.xdebug.yml ./
+mv ./symfony-docker-compose/.docker ./.docker
+mv ./symfony-docker-compose/Makefile ./Makefile
+mv ./symfony-docker-compose/docker-compose.yml ./docker-compose.yml
+mv ./symfony-docker-compose/docker-compose.xdebug.yml ./docker-compose.xdebug.yml
 rm -rf ./symfony-docker-compose
